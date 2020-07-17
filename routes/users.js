7ddc40f3d71c88556f0951d450f0402c84018551
Bhavesh <bhavesh.ittadwar16@vit.edu>
2020-07-17 11:19:21 +0530

    Profile route added & Protected routes changed

--- routes/users.js
+++ routes/users.js
@@ -18,13 +18,16 @@ router.post("/login-admin", async (req, res) => {});
 //Super Admin Login Route
 router.post("/login-super-admin", async (req, res) => {});
 
+//Profile Route
+router.get('/profile', (req, res) => {});
+
 //User Protected Route
-router.post("/user-profile", async (req, res) => {});
+router.post("/user-protected", async (req, res) => {});
 
 //Admin Protected Route
-router.post("/admin-profile", async (req, res) => {});
+router.post("/admin-protected", async (req, res) => {});
 
 //Super Admin Protected Route
-router.post("/super-admin-profile", async (req, res) => {});
+router.post("/super-admin-protected", async (req, res) => {});
 
 module.exports = router;
